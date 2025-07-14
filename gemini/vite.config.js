16e3e2ccb9e94e6fd6c9b42135d338de35e629aa
prabhakar <prabhakarpandey7061@gmail.com>
2025-07-15 02:12:38 +0530

    change somthing in vite.config

--- gemini/vite.config.js
+++ gemini/vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+   base: './',
   plugins: [tailwindcss()],
 })
